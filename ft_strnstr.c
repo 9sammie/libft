e27cc8607fd91233fbb210aee4c9b72205a1851a
Mathis Ballet <maballet@z1r11p4.42lyon.fr>
2024-11-14 18:05:37 +0100

    maj strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,35 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:10:25 by maballet          #+#    #+#             */
-/*   Updated: 2024/11/14 17:15:22 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2024/11/14 18:04:36 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *strnstr(const char *big, const char *little, size_t len)
+char    *ft_strnstr(const char *big, const char *little, size_t len)
 {
     size_t i;
     size_t l;
+    char    *large;
+    char    *mini;
 
+    large = (char *)big;
+    mini = (char *)little;
     i = 0;
     l = 0;
-    if(little[i] == 0)
-        return(big[i]);
-    while(big[i] && i < len)
+    if(mini[i] == 0)
+        return(large);
+    while(large[i] && i < len)
     {
-        if(big[i] == little[i])
+        while(large[i + l] == mini[l] && l < ft_strlen(mini))
         {
-            while(big[i] == little[i] && l < ft_strlen(little))
-            {
-                l++;
-            }
-            if(l == ft_strlen(little) - 1)
-                return(i);
-            else
-                l = 0;
+            l++;
         }
+        if(l == ft_strlen(mini))
+            return(&large[i]);
+        else
+            l = 0;
         i++;
     }
     return(0);
@@ -41,9 +42,9 @@ char    *strnstr(const char *big, const char *little, size_t len)
 
 int main()
 {
-    const char big[] = "bjonjoureuh";
+    const char big[] = "bonjoure";
     const char little[] = "jour";
-    size_t len = 12;
+    size_t len = 9;
 
     printf("func: %s\nMine: %s\n", strnstr(big, little, len), ft_strnstr(big, little, len));
     return(0);
