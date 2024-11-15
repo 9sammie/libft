762529c6cee7a9d5666e3a3fb9c7a67e3b3b8395
Mathis Ballet <maballet@z1r11p3.42lyon.fr>
2024-11-15 16:38:18 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 10:12:29 by maballet          #+#    #+#             */
-/*   Updated: 2024/11/15 16:29:14 by maballet         ###   ########lyon.fr   */
+/*   Created: 2024/11/15 13:36:01 by maballet          #+#    #+#             */
+/*   Updated: 2024/11/15 16:37:23 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <stdio.h>
-//#include <string.h>
 
-void    ft_bzero(void *s, size_t n)
+void    *ft_calloc(size_t nmemb, size_t size)
 {
-    size_t i;
+    size_t  i;
+    void *array;
 
     i = 0;
-    while (i < n)
-    {
-        *(unsigned char *) (s + i) = '\0';
-        i++;
-    }
+    if(nmemb * size == 0 || size * nmemb / size != nmemb)
+        return (0);
+    array = malloc(size * nmemb);
+    if(array == NULL)
+        return (0);
+    ft_bzero(array, size);
+    return (array);
 }
 /*
 int main()
 {
-    char str[] = "hello world";
-    char ori[] = "hello world";
+    size_t nmemb;
+    size_t size;
 
-    printf("before  : %s\n", str);
-    ft_bzero(str, 6);
-    printf("mine    : %s\n", str);
-    bzero(ori, 6);
-    printf("orig    : %s\n", ori);
+    nmemb = 6;
+    size = 0;
+    printf("true: %p\n", calloc(nmemb, size));
+    printf("mine: %p\n", ft_calloc(nmemb, size));
     return(0);
 }
 */
