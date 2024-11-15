c1e116347b2c3ac28429586942362c96ab9ef19c
Mathis Ballet <maballet@z1r11p3.42lyon.fr>
2024-11-15 16:53:46 +0100

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 16:42:41 by maballet          #+#    #+#             */
+/*   Updated: 2024/11/15 16:52:58 by maballet         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strdup(const char *s)
+{
+    int i;
+    char    *dup;
+
+    i = 0;
+    dup = malloc(ft_strlen((char *)s));
+    if(dup == NULL)
+        return (NULL);
+    while(i < ft_strlen((char *)s))
+    {
+        dup[i] = s[i];
+        i++;
+    }
+    return(dup);
+}
+
+int main()
+{
+    const char  s[] = "bonjour";
+
+    printf("true: %s\n", strdup(s));
+    printf("mine: %s\n", ft_strdup(s));
+    return(0);
+}
