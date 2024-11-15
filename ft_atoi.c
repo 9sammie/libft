de60a6ceecc70c11cf937d5a8350592d0f280e6e
Mathis Ballet <maballet@z1r11p3.42lyon.fr>
2024-11-15 13:24:24 +0100

    atoi et memmove

---
+++ ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 18:09:17 by maballet          #+#    #+#             */
+/*   Updated: 2024/11/15 13:23:24 by maballet         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_atoi(const char *nptr)
+{
+    int i;
+    int sign;
+    int result;
+
+    i = 0;
+    sign = 1;
+    result = 0;
+    while(nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+        i++;
+    if(nptr[i] == '+' || nptr[i] == '-')
+    {
+        if(nptr[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while(nptr[i] >= '0' && nptr[i] <= '9')
+    {
+        result = result * 10 + (nptr[i] - '0');
+        i++;
+    }
+    return(result * sign);
+}
+/*
+int main()
+{
+    const char  nptr[] = "    -47";
+    printf("mine: %d\ntrue: %d\n", ft_atoi(nptr), atoi(nptr));
+    return(0);
+}
+*/
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:12:29 by maballet          #+#    #+#             */
-/*   Updated: 2024/11/14 14:55:01 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2024/11/15 12:45:44 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void    *ft_memmove(void *dest, const void *src, size_t n)
     i = 0;
     s = (unsigned char *)src;
     d = (unsigned char *)dest;
-    if(d[i] == '\0' || s[i] == '\0')
-        return(0);
-    while(i <n)
+//    if(d[i] == '\0' || s[i] == '\0')
+//        return(0);
+    while(i < n)
     {
         if(d > s)
             d[i] = s[i];
@@ -54,14 +54,14 @@ void    *ft_memmove(void *dest, const void *src, size_t n)
         d[n] = s[n];
     }
     return(dest);
-}
+}*/
 
 int main()
 {
     char str1[] = "hello world";
     char str2[] = "hello world";
-    char destm[] = "salutations";
-    char desto[] = "salutations";
+    char destm[] = "";
+    char desto[] = "";
     size_t n = 6;
 
     printf("src     : %s\n", str1);
@@ -71,4 +71,3 @@ int main()
     printf("func    : %s\n", desto);
     return(0);
 }
-*/
